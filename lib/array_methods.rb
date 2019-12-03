3a60a4a3d3dea904fc1ac05e00a3a241c9e35e47
Nathan Brickett <natey37@gmail.com>
2019-12-03 16:19:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,10 +2,9 @@ def find_element_index(array, value_to_find)
   array.each do |x|
     if value_to_find == x 
       return array.index(x)
-    else 
-      return nil
     end 
   end 
+    return nil 
 end
 
 def find_max_value(array)
